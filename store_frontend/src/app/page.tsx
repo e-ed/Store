50f32782e274576938de52ed8cb2a62b38215bc4
Felipe <61094586+nascimento-felipe@users.noreply.github.com>
2023-08-25 19:50:13 -0300

    feat: main page done

--- store_frontend/src/app/page.tsx
+++ store_frontend/src/app/page.tsx
@@ -5,12 +5,43 @@ import ButtonDelete from "@/components/mine/buttonDelete";
 import ButtonGet from "@/components/mine/buttonGet";
 import ButtonUpdate from "@/components/mine/buttonUpdate";
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { api } from "@/lib/axios";
 import { BadgeCheck } from "lucide-react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
   const [showAlert, setShowAlert] = useState(false);
   const [message, setMessage] = useState("");
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    getAllProducts().then((r) => {
+      setProducts(r);
+    });
+  }, []);
+
+  async function getAllProducts() {
+    try {
+      const result = await api.get("/product");
+
+      if (!result) {
+        console.log("deu erro chefia");
+        return;
+      }
+
+      return result.data;
+    } catch (error) {
+      console.error(error);
+    }
+  }
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-around p-24">
@@ -26,9 +57,38 @@ export default function Home() {
 
       <section className="flex gap-4">
         <ButtonCreate setMessage={setMessage} showAlert={setShowAlert} />
-        <ButtonGet />
+        <ButtonGet
+          setMessage={setMessage}
+          showAlert={setShowAlert}
+          showFoundProduct={setProducts}
+        />
         <ButtonUpdate />
         <ButtonDelete setMessage={setMessage} showAlert={setShowAlert} />
+      </section>
+
+      <section>
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Name</TableHead>
+              <TableHead>Description</TableHead>
+              <TableHead>Category</TableHead>
+              <TableHead>Price</TableHead>
+              <TableHead>Stock Quantity</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {products.map((product, i) => (
+              <TableRow key={i}>
+                <TableCell>{product.productName}</TableCell>
+                <TableCell>{product.description}</TableCell>
+                <TableCell>{product.category}</TableCell>
+                <TableCell>{product.price}</TableCell>
+                <TableCell>{product.stockQuantity}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
       </section>
     </main>
   );
